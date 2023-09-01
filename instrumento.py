89c190010cf00a481032e388fa1eb6c8570c304a
Sebastian19Charry <142014728+Sebastian19Charry@users.noreply.github.com>
2023-09-01 07:38:42 -0500

    Add files via upload

---
+++ instrumento.py
@@ -0,0 +1,7 @@
+class Instrumento:
+    def instrumento(self, guitarra, piano, flauta, bajo, bateria):
+        gt = guitarra
+        pn = piano
+        ft = flauta
+        bj = bajo
+        bt = bateria
---
+++ musico.py
@@ -0,0 +1,14 @@
+from instrumento import *
+class Musico:
+    def __init__(self, instrumento):
+        self.instrumento = instrumento
+
+    def tocar_instrumento(self):
+        self.instrumento.tocar_instrumento()
+
+    def afinar_instrumento(self):
+        self.instrumento.afinar_instrumento()
+    
+
+
+    
